7eaab33d8753b10ebc0646655ceaf92c784062a5
Prabhakar Lal <154450616+Prabhakar0tenn@users.noreply.github.com>
2025-09-02 23:30:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";  // agar tum CSS file use karna chaho
+  // agar tum CSS file use karna chaho
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
